62d6a98d826502959e311f73a9069adca898f8f3
davidkankowski <davidkanko12@gmail.com>
2025-12-16 14:29:30 -0800

    Populated Property Address Data

--- Project.sql
+++ Project.sql
@@ -26,3 +26,29 @@ Update NashvilleHousing
 SET SaleDateConverted = CONVERT(Date,SaleDate)
 
 -------------------------------------------------------------------------
+
+-- Populate Property Address data
+
+Select *
+From PortfolioProject.dbo.NashvilleHousing
+--Where PropertyAddress is null
+order by ParcelID
+
+-- Duplicated ParcelID's have same PropertyAddress, can resolve 35 Null values by matching duplicates.
+Select a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL(a.PropertyAddress, b.PropertyAddress)
+From PortfolioProject.dbo.NashvilleHousing a
+JOIN PortfolioProject.dbo.NashvilleHousing b
+    on a.ParcelID = b.ParcelID
+    AND a.[UniqueID ] <> b.[UniqueID ]
+Where a.PropertyAddress is null
+
+-- 35 Null results resolved.
+Update a
+SET PropertyAddress = ISNULL(a.PropertyAddress, b.PropertyAddress)
+From PortfolioProject.dbo.NashvilleHousing a
+JOIN PortfolioProject.dbo.NashvilleHousing b
+    on a.ParcelID = b.ParcelID
+    AND a.[UniqueID ] <> b.[UniqueID ]
+Where a.PropertyAddress is null
+
+-------------------------------------------------------------------------
