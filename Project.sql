4ccb308171a7a7974c0288f0871b48c1306b40c7
davidkankowski <davidkanko12@gmail.com>
2025-12-18 15:28:36 -0800

    Separated address into individual columns (Address, City, State).

--- Project.sql
+++ Project.sql
@@ -52,3 +52,64 @@ JOIN PortfolioProject.dbo.NashvilleHousing b
 Where a.PropertyAddress is null
 
 -------------------------------------------------------------------------
+
+-- Breaking out Address into Individual Columns (Address, City, State)
+
+Select PropertyAddress
+From PortfolioProject.dbo.NashvilleHousing
+--Where PropertyAddress is null
+--order by ParcelID
+
+-- Create two PropertyAddress substrings to separate address from city.
+SELECT
+SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1 ) as Address
+, SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) +1 , LEN(PropertyAddress)) as Address
+From PortfolioProject.dbo.NashvilleHousing
+
+-- Add 2 new columns for address and city individually.
+
+ALTER TABLE NashvilleHousing
+Add PropertySplitAddress Nvarchar(255);
+
+Update NashvilleHousing
+SET PropertySplitAddress = SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1 )
+
+ALTER TABLE NashvilleHousing
+Add PropertySplitCity Nvarchar(255);
+
+Update NashvilleHousing
+SET PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) +1 , LEN(PropertyAddress))
+
+
+-- Separate owner address into three parts: address, city, state.
+
+Select OwnerAddress
+From PortfolioProject.dbo.NashvilleHousing
+
+SELECT
+PARSENAME(REPLACE(OwnerAddress, ',', '.'), 3)
+, PARSENAME(REPLACE(OwnerAddress, ',', '.'), 2)
+, PARSENAME(REPLACE(OwnerAddress, ',', '.'), 1)
+From PortfolioProject.dbo.NashvilleHousing
+
+-- Add 3 new columns for respective owner address parts: address, city, state.
+
+ALTER TABLE NashvilleHousing
+Add OwnerSplitAddress Nvarchar(255);
+
+Update NashvilleHousing
+SET OwnerSplitAddress = PARSENAME(REPLACE(OwnerAddress, ',', '.'), 3)
+
+ALTER TABLE NashvilleHousing
+Add OwnerSplitCity Nvarchar(255);
+
+Update NashvilleHousing
+SET OwnerSplitCity = PARSENAME(REPLACE(OwnerAddress, ',', '.'), 2)
+
+ALTER TABLE NashvilleHousing
+Add OwnerSplitState Nvarchar(255);
+
+Update NashvilleHousing
+SET OwnerSplitState = PARSENAME(REPLACE(OwnerAddress, ',', '.'), 1)
+
+-------------------------------------------------------------------------
