6aa677be219ad8ba1e05e54efb3b59b0ca649094
davidkankowski <davidkanko12@gmail.com>
2025-12-18 15:40:45 -0800

    Changed Y and N to Yes and No in SoldAsVacant field

--- Project.sql
+++ Project.sql
@@ -113,3 +113,27 @@ Update NashvilleHousing
 SET OwnerSplitState = PARSENAME(REPLACE(OwnerAddress, ',', '.'), 1)
 
 -------------------------------------------------------------------------
+
+-- Change Y and N to Yes and No in "Sold as Vacant" field
+
+Select Distinct(SoldAsVacant), Count(SoldAsVacant)
+From PortfolioProject.dbo.NashvilleHousing
+Group by SoldAsVacant
+Order by 2
+
+
+Select SoldAsVacant
+, CASE When SoldAsVacant = 'Y' THEN 'Yes'
+       When SoldAsVacant = 'N' THEN 'No'
+       ELSE SoldAsVacant
+       END
+From PortfolioProject.dbo.NashvilleHousing
+
+
+Update NashvilleHousing
+SET SoldAsVacant = CASE When SoldAsVacant = 'Y' THEN 'Yes'
+       When SoldAsVacant = 'N' THEN 'No'
+       ELSE SoldAsVacant
+       END
+
+-------------------------------------------------------------------------
