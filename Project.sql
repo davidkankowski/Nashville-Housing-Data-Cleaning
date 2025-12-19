9144f199c96f41e712083da8f2cdaef73a1cfb2f
davidkankowski <davidkanko12@gmail.com>
2025-12-18 16:51:23 -0800

    Deleted Unused Columns

--- Project.sql
+++ Project.sql
@@ -160,4 +160,13 @@ Where row_num > 1
 
 -------------------------------------------------------------------------
 
+-- Delete Unused Columns
 
+Select *
+From PortfolioProject.dbo.NashvilleHousing
+
+ALTER TABLE PortfolioProject.dbo.NashvilleHousing
+DROP COLUMN OwnerAddress, TaxDistrict, PropertyAddress
+
+ALTER TABLE PortfolioProject.dbo.NashvilleHousing
+DROP COLUMN SaleDate
